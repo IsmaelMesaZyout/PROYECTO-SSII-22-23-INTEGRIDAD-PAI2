6d57433c844e58cbf47372ab1c1e0dc3ccb15210
Ismael <ismaelmesazyout@gmail.com>
2022-10-13 22:21:03 +0200

    Terminado

--- conexionServer.py
+++ conexionServer.py
@@ -1,14 +1,14 @@
 import socket
 import hmac
 import random
-import aperturaBaseDatos
+import nonces
 from datetime import datetime
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 def l(v):
     clave = "1234"
-    hmacServer = hmac.hmacFuncion(gen_prueba_random(v)[0], clave)  # type: ignore
+    hmacServer = hmac.hmacFuncion(gen_prueba_random(v), clave)  # type: ignore
     return hmacServer
 
 def gen_prueba_random(cadena):
@@ -17,7 +17,7 @@ def gen_prueba_random(cadena):
     if(v > 0.65):
         x[2] = random.randint(0,1000000)
         cadena = x[0] + " " + x[1] + " " + str(x[2]) + " " + str(x[3])
-    return cadena, str(x[3])
+    return cadena
 
 def func(msgInput,keyInput):
     paquete = []
@@ -35,23 +35,11 @@ for line in file:
 num_pruebas = config[0]
 file.close()
 
-
-
-def run_query(query=''):    
-    cursor = aperturaBaseDatos.conexion.cursor()
-    cursor.execute(query)          # Ejecutar una consulta 
-    if query.upper().startswith('SELECT'): 
-        data = cursor.fetchall()   # Traer los resultados de un select 
-    else:               # Hacer efectiva la escritura de datos 
-        data = None
-    return data
-
-cursor = aperturaBaseDatos.conexion.cursor()
 cont = 0
 archivo = "./log/" + str(datetime.now().strftime('%Y_%m'))
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
-    while(True):
+    for i in range(int(num_pruebas)):
         s.listen()
         conn, addr = s.accept()
         with conn:
@@ -74,31 +62,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 file.close()
                 
                 k = p.split(" ")[3]
-                print(k)
-                if(cont <= 0):
-                    cursor.execute("INSERT INTO tablepai2(Nonce, NumeroCuenta) VALUES (%s,%s)",(k, 65478))
-                    aperturaBaseDatos.conexion.commit()
-                    cont += 1
-                else:
-                    file = open(archivo + ".txt", "a")
-                    val = "SELECT * FROM tablepai2"
-                    val = run_query(val)
-                    print(val)
-                    n = False
-                    for m in val or []:
-                        if(m[0] == k):
-                            print("FALLO, HA OCURRIDO UN REPLAY")
-                            file.write("Dia " + str(datetime.now().strftime('%d')) + " Hora " + str(
-                            datetime.now().strftime('%H:%M')) + ": FALLOREPLAY - Se ha realizado un replay\n")
-                            n = True
-                    if(n==False):
-                        cursor.execute("INSERT INTO tablepai2(Nonce, NumeroCuenta) VALUES (%s,%s)",(k, 65478))
-                        aperturaBaseDatos.conexion.commit()
-                        print("No ha habido replay y Insertado nuevo nonce en base de datos")
-                        file.write("Dia " + str(datetime.now().strftime('%d')) + " Hora " + str(datetime.now(
-                        ).strftime('%H:%M')) + ": ACIERTO - No se ha realizado un replay\n")  
-                    file.close()
-
+                nonces.sel(k,cont)
+                cont+=1
 
 file = open(archivo + ".txt", "r")
 cont = 0
--- prueba.py
+++ prueba.py
@@ -1,15 +1,1 @@
-import aperturaBaseDatos
-
-def run_query(query=''):    
-    cursor = aperturaBaseDatos.conexion.cursor()
-    cursor.execute(query)          # Ejecutar una consulta 
-    if query.upper().startswith('SELECT'): 
-        data = cursor.fetchall()   # Traer los resultados de un select 
-    else:               # Hacer efectiva la escritura de datos 
-        data = None
-    return data
-
-def sel():
-    val1 = "SELECT * FROM tablepai2"
-    val1 = run_query(val1) 
-    return val1
+lista = []
